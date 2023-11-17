c2b2177af8d1104428e94be382b9e448184c6925
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-11-17 17:22:14 +0100

    Update p3_the_process_ring_v2
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- additional/p3_the_process_ring_v2.erl
+++ additional/p3_the_process_ring_v2.erl
@@ -1,41 +1,53 @@
 -module(p3_the_process_ring_v2).
 
--export([start/0, start/3, create/3, loop/2]).
+-export([start/0, stop/0]).
 
 start() ->
   start(2, 5, "Hello").
 
 start(M, N, Msg) ->
-  PidHandler = whereis(first_node),
-  case PidHandler of
+  register(handler, self()),
+  MaybeFirstNode = whereis(first_node),
+  case MaybeFirstNode of
     undefined ->
-      % register(first_node, spawn(fun() -> create(N, 1, self()) end)), % DOES NOT WORK, maybe beacause of the self() is internal to the lambda
-      register(first_node, spawn(?MODULE, create, [N, 1, self()])),
-      io:format("___START: Handler start at ~p~n", [self()]);
+      FirstNode = spawn(fun() -> create(M - 1, N - 1) end),
+      register(first_node, FirstNode),
+      io:format("___START: First node started at ~p~n", [FirstNode]);
     _ ->
-      io:format("___START: Handler already started at ~p~n", [PidHandler])
+      stop(), start(M, N, Msg)
   end,
+  io:format("___START: Waiting for ring creation~n"),
   receive
-    ring_created  ->
-      io:format("___START: The ring is created~n"),
-      ok;
-    Any ->
-      io:format("___START: Unexpected message ~p~n", [Any]),
-      exit(unexpected_message)
-  after 10000 ->
-    unregister(first_node), % DOES NOT WORK
-    exit(ring_not_created)
+    ring_created -> ok;
+    _ -> io:format("___START: Unexpected message~n")
+    after 5000 -> exit(after1000)
+  end,
+  io:format("___START: Ring created~n"),
+  first_node ! {M, Msg}.
+
+stop() ->
+  io:format("___STOP: Stopping ring~n"),
+  exit(whereis(first_node), kill),
+  unregister(first_node).
+
+create(M, 0) ->
+  % first_node ! ring_created,
+  handler ! ring_created,
+  io:format("_______CREATE: Ring created~n"),
+  loop(first_node, M);
+create(M, N) ->
+  Pid = spawn(fun() -> create(M, N - 1) end),
+  io:format("_______CREATE: Node ~p created at ~p~n", [N, Pid]),
+  loop(Pid,  M).
+
+loop(Next, M) ->
+  receive
+    {0, Msg} ->
+      io:format("______LOOP: Node ~p is sending ~p to ~p -- Last message~n", [self(), Msg, Next]),
+      Next ! {0, Msg};
+    {_, Msg} ->
+      io:format("______LOOP: Node ~p is sending ~p to ~p~n", [self(), Msg, Next]),
+      Next ! {M, Msg},
+      loop(Next, M - 1)
   end.
-  % Handle next steps
 
-create(1, S, Handler) ->
-  io:format("___CREATE: Node ~p (id: ~p) point to ~p (id: ~p)~n", [self(), S, whereis(first_node), 1]),
-  Handler ! ring_created;
-create(N, S, Handler) ->
-  % Next = spawn(fun() -> create(N - 1, S + 1, Handler) end), % DOES NOT WORK
-  Next = spawn(?MODULE, create, [N - 1, S + 1, Handler]),
-  io:format("___CREATE: Node ~p (id: ~p) point to ~p (id: ~p)~n", [self(), S, Next, S + 1]),
-  loop(Next, N).
-
-loop(Next, N) ->
-  ok.
