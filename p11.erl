f65f428f120d66f510ff1fa9ead534d0f304b988
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-11-19 13:33:03 +0100

    Add commet p11
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- p11.erl
+++ p11.erl
@@ -1,13 +1,19 @@
+% (*) Modified run-length encoding.
+%
+% Modify the result of problem P10 in such a way that if an element has no
+% duplicates it is simply copied into the result list. Only elements with
+% duplicates are transferred as (N, E) terms.
+
 -module(p11).
 
 -export([encodeModified/1, start/0]).
 
 encodeModified(L) ->
   X = pack(L),
-  lists:map(fun(E) -> 
+  lists:map(fun(E) ->
               case length(E) of
                 1 -> hd(E);
-                _ -> {length(E), hd(E)} 
+                _ -> {length(E), hd(E)}
               end
             end, X).
 
